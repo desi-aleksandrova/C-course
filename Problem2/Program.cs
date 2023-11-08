74b56ed0b5aeb227adaecb4e8e6f35f0f8c71067
Desislava Aleksandrova <137012377+desi-aleksandrova@users.noreply.github.com>
2023-11-09 01:42:54 +0200

    Add files via upload

---
+++ Problem2/Program.cs
@@ -0,0 +1,29 @@
+﻿string inputArray = Console.ReadLine();
+string[] numbersStr = inputArray.Split(' ');
+
+if (int.TryParse(Console.ReadLine(), out int N))
+{
+    if (N >= 1 && N <= numbersStr.Length)
+    {
+        int[] numbers = new int[numbersStr.Length];
+
+        for (int i = 0; i < numbersStr.Length; i++)
+        {
+            if (int.TryParse(numbersStr[i], out int number))
+            {
+                numbers[i] = number;
+            }
+        }
+
+        double sum = 0;
+
+        for (int i = numbers.Length - N; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        double average = sum / N;
+
+        Console.WriteLine(average.ToString("F2"));
+    }
+}
