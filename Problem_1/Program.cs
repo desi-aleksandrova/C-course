afe936e78035fb84283ccfdedaa78b2d9fe11461
Desislava Aleksandrova <137012377+desi-aleksandrova@users.noreply.github.com>
2023-11-09 01:42:00 +0200

    Add files via upload

---
+++ Problem_1/Program.cs
@@ -0,0 +1,25 @@
+﻿ if (int.TryParse(Console.ReadLine(), out int number) && number >= 0)
+    while (number > 0)
+                {
+                    int digit = number % 10;
+                    string digitName = GetDigitName(digit);
+                    Console.WriteLine(digitName);
+                    number /= 10;
+                }
+    static string GetDigitName(int digit)
+{
+    switch (digit)
+    {
+        case 1: return "one";
+        case 2: return "two";
+        case 3: return "three";
+        case 4: return "four";
+        case 5: return "five";
+        case 6: return "six";
+        case 7: return "seven";
+        case 8: return "eight";
+        case 9: return "nine";
+        default: return "unknown";
+    }
+}
+ 
